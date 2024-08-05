c081d5e9c977891298b44b9d5091dc31dfd35344
douglasdl <douglas_san@hotmail.com>
2024-08-05 18:32:36 -0300

    test: auto format test

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <h1 className="px-4 text-red-500">Home Page</h1>
-                        <Button>Test</Button>
+                      <Button>Test</Button>
     </>
   );
 }
