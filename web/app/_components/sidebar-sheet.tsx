ea100aea2d754b44e8bfb862df701db0a6b08aad
douglasdl <douglas_san@hotmail.com>
2024-08-08 19:47:27 -0300

    feat: add login popup and Google button

--- web/app/_components/sidebar-sheet.tsx
+++ web/app/_components/sidebar-sheet.tsx
@@ -1,10 +1,11 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
 import Image from "next/image";
 import { quickSearchOptions } from "../_constants/search";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
 
 export function SidebarSheet() {
   return (
@@ -13,7 +14,29 @@ export function SidebarSheet() {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-4 py-5 border-b border-solid">
+      <div className="flex items-center justify-between gap-3 py-5 border-b border-solid">
+        <h2 className="font-bold">Olá. Faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant="outline" className="gap-2 font-bold">
+              <Image src="/google.svg" width={18} height={18} alt="Fazer login com Google" />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+      {/*
         <Avatar>
           <AvatarImage src="http://www.github.com/douglasdl.png" />
         </Avatar>
@@ -22,7 +45,8 @@ export function SidebarSheet() {
           <p className="font-bold">Douglas Dias Leal</p>
           <p className="text-gray-500 text-sm">douglas_san@hotmail.com</p>
         </div>
-      </div>
+        */}
+      </div> 
       
       <div className="flex flex-col gap-4 p-5 border-b border-solid">
         <SheetClose asChild>
